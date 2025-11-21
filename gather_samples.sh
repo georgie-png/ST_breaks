a56c631316475385f5831c8a35e15e2982e4555b
Georgie <simmsgeorge7@gmail.com>
2025-11-21 08:03:48 +0000

    bug fixed hopefully

--- gather_samples.sh
+++ gather_samples.sh
@@ -4,23 +4,23 @@ output_file="strudel.json"
 
 rm $output_file
 
-echo "{\"_base\":\"https://raw.githubusercontent.com/georgie-png/ST_breaks/main/\"," >> $output_file
+jsonData="{\"_base\":\"https://raw.githubusercontent.com/georgie-png/ST_breaks/main/\"" >> $output_file
 
 for file in ./*/*.wav
 do
   if [[ "$file" != "./${output_file}" ]]
     then
+        echo "${jsonData}," >> ${output_file}
 
         xbase=${file##*/}
         xpref=${xbase%.*}
         name=${xpref%%-*}
         path=${file#"./"}
 
-        echo "\"${name}\":[\"${path}\"]," >> ${output_file}
+        jsonData="\"${name}\":[\"${path}\"]"
   fi
  
-
-
 done
 
+echo $jsonData >> ${output_file}
 echo "}" >> $output_file
